621d336766b36b5e0da50f991cf1d115feabfb36
Mike Guedes <mike.guedes.ms@gmail.com>
2024-11-07 00:08:06 -0300

    Atividades do dia 6

--- dia 6/index.js
+++ dia 6/index.js
@@ -5,21 +5,28 @@ for(let contador = 0; contador <= numero; contador++){
 }
 
     
-    for(let contador = 0; contador <= 50; contador+= 5){
+    
+
+for(let contador = 0; contador <= 50; contador+= 5){
         console.log(contador)
 }
 
 
-        for(let contador = 50; contador >= 0; contador-= 5){
+        
+
+for(let contador = 50; contador >= 0; contador-= 5){
              console.log(contador)
 }
 
 
 
-let tabuada = Number(prompt("escolha a tabuada que deseja"))
 
-for(let contador = 0; contador = <= 10; contador++ ){
-    console.log(tabuada + " x " + contador + " = " + tabuada*0)
+let tabuada = parseInt(prompt("escolha a tabuada que deseja"))
+
+for(let i = tabuada; i <= tabuada + 2; i++ ) {
+    console.log("tabuada do numero: " + i)
+    for(let j = 0; j <= 10; j++) {
+        console.log(i + " x " + j + " = " + (i*j))
+ }
 }
-
 
